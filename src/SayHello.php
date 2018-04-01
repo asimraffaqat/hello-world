ac9910b980e2138fb7e7c8a9e5af3342663c9b54
Asim Raffaqat <asimraffaqat8@gmail.com>
2018-04-01 11:01:38 +0500

    new

---
+++ src/SayHello.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace asimraff\Hello\World;
+/**
+ * Created by PhpStorm.
+ * User: Asim Raffaqat
+ * Date: 4/1/2018
+ * Time: 10:56 AM
+ */
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello from AsimRaff!';
+    }
+}
